85bffe33f5a391d8670ae12292eed1bf566ab8db
Mikhail Tarasov (Timcore) <mikhailtarasov2016@yandex.ru>
2024-12-20 14:25:53 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
+#!/bin/bash
+
+# Создаем основную структуру директорий
 mkdir -p timfuzzer/{lib,bin,config,modules,payloads,reports}
-touch timfuzzer/Gemfile
+
+# Создаем Gemfile
+cat > timfuzzer/Gemfile << 'EOL'
+source 'https://rubygems.org'
+
+gem 'nokogiri'
+gem 'mechanize'
+gem 'thor'
+gem 'colorize'
+gem 'parallel'
+gem 'json'
+EOL
+
+# Создаем исполняемый файл
 touch timfuzzer/bin/timfuzzer
-chmod +x timfuzzer/bin/timfuzzer 
+chmod +x timfuzzer/bin/timfuzzer
+
+# Создаем директории для модулей
+mkdir -p timfuzzer/lib/timfuzzer
+
+# Устанавливаем права
+chmod -R 755 timfuzzer
